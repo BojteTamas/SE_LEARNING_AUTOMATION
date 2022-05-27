8079dbe38a2426dab1b6479620f7791d46447fee
Raluca <ralu2491@gmail.com>
2022-05-27 12:22:22 +0300

    27MayeTest

--- src/test/java/RalucaTest.java
+++ src/test/java/RalucaTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 import java.util.Objects;
 
+/**
 public class RalucaTest {
 
 
@@ -78,13 +79,13 @@ public class RalucaTest {
 
     @Test
     public void thirdTest(){
-       /** WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
         chromeDriver = new ChromeDriver();
-        **/
+
         //chromeDriver.get("https://testautomationu.applitools.com/");
 
-       // chromeDriver.navigate().refresh();
-        chromeDriver.get(chromeDriver.getCurrentUrl());
+        chromeDriver.navigate().refresh();
+       // chromeDriver.get(chromeDriver.getCurrentUrl());
         waitSomeTime(6);
 
     }
@@ -96,6 +97,36 @@ public class RalucaTest {
         colorTheElementInRed(profilebutton);
         profilebutton.click();
         waitSomeTime(1);
+    }
+
+
+   @Test
+   public void forthTest(){
+       WebElement profilebutton =
+               chromeDriver.findElement(By.cssSelector("#app > div.theme-container.no-sidebar > header > div.links > div > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)"));
+       colorTheElementInRed(profilebutton);
+       profilebutton.click();
+       waitSomeTime(3);
+         profilebutton.click();
+        waitSomeTime(1);
+        profilebutton.click();
+        waitSomeTime(1);
+
+
+       chromeDriver.navigate().refresh();
+       waitSomeTime(1);
+   }
+
+    @Test
+    public void fifthTest(){
+
+
+        WebElement editprofbutton =
+                chromeDriver.findElement(By.cssSelector("#app > div.theme-container.no-sidebar > div.page > div.page-edit"));
+        colorTheElementInRed(editprofbutton);
+        editprofbutton.click();
+        waitSomeTime(2);
+        //
     }
 
 
@@ -114,6 +145,166 @@ public class RalucaTest {
     public void colorTheElementInRed(final WebElement webElement) {
 
         chromeDriver.executeScript("arguments[0].style.border='3px solid red'", webElement);
+    }
+
+    public void colorTheElement(final WebElement webElement, String color) {
+
+        chromeDriver.executeScript(
+                String.format("arguments[0].style.border='3px solid %s'", color), webElement);
+    }
+
+
+}
+
+**/
+
+
+
+/** WebDriverManager.chromedriver().setup();
+ chromeDriver = new ChromeDriver();
+ **/
+
+
+public class RalucaTest {
+
+
+    static ChromeDriver chromeDriver;
+
+    @BeforeAll
+    public static void setup() {
+        WebDriverManager.chromedriver().setup();
+        chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://testautomationu.applitools.com/");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        chromeDriver.quit();
+    }
+
+    @Test
+    public void firstTest() {
+
+        WebElement enrollbutton =
+                chromeDriver.findElement(By.cssSelector("#app > div.theme-container.no-sidebar > header > div.links > div > div:nth-child(1) > div > a:nth-child(1)"));
+        colorTheElementInRed(enrollbutton);
+        waitSomeTime(1);
+        enrollbutton.click();
+        waitSomeTime(1);
+
+        WebElement loginbutton =
+                chromeDriver.findElement(By.cssSelector("#firebaseui-auth-container > div > div.firebaseui-card-content > form > ul > li:nth-child(2) > button > span.firebaseui-idp-text.firebaseui-idp-text-long"));
+        colorTheElementInRed(loginbutton);
+        waitSomeTime(1);
+        loginbutton.click();
+        waitSomeTime(1);
+
+        //    chromeDriver.navigate().refresh();
+        //   waitSomeTime(2);
+
+    }
+
+    @Test
+    public void secondTest() {
+
+        WebElement emailInput =
+                chromeDriver.findElement(By.cssSelector("#firebaseui-auth-container > div > form > div.firebaseui-card-content > div > div.firebaseui-textfield.mdl-textfield.mdl-js-textfield.mdl-textfield--floating-label.is-upgraded > input")); // input email
+        colorTheElementInRed(emailInput);
+        emailInput.sendKeys("raluca.ion@hotmail.com");
+        waitSomeTime(2);
+
+        WebElement nextbutton =
+                chromeDriver.findElement(By.cssSelector("#firebaseui-auth-container > div > form > div.firebaseui-card-actions > div > button.firebaseui-id-submit.firebaseui-button.mdl-button.mdl-js-button.mdl-button--raised.mdl-button--colored"));
+        colorTheElementInRed(nextbutton);
+        nextbutton.click();
+        waitSomeTime(1);
+
+        WebElement passInput =
+                chromeDriver.findElement(By.cssSelector("#firebaseui-auth-container > div > form > div.firebaseui-card-content > div:nth-child(3) > input"));
+        colorTheElementInRed(passInput);
+        passInput.sendKeys("Hell0There!");
+        waitSomeTime(1);
+
+        WebElement savebutton =
+                chromeDriver.findElement(By.cssSelector("#firebaseui-auth-container > div > form > div.firebaseui-card-actions > div.firebaseui-form-actions > button"));
+        colorTheElementInRed(savebutton);
+        savebutton.click();
+        waitSomeTime(1);
+
+        chromeDriver.navigate().refresh();
+        waitSomeTime(2);
+
+
+
+    }
+
+
+    @Test
+    public void thirdTest(){
+        /** WebDriverManager.chromedriver().setup();
+         chromeDriver = new ChromeDriver();
+         **/
+        //chromeDriver.get("https://testautomationu.applitools.com/");
+
+
+        //   chromeDriver.get(chromeDriver.getCurrentUrl());
+        //   waitSomeTime(2);
+        chromeDriver.navigate().refresh();
+        waitSomeTime(2);
+        //   chromeDriver.navigate().refresh();
+        //  waitSomeTime(2);
+
+    }
+
+    @Test
+    public void forthTest(){
+        WebElement profilebutton =
+                chromeDriver.findElement(By.cssSelector("#app > div.theme-container.no-sidebar > header > div.links > div > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)"));
+        colorTheElementInRed(profilebutton);
+        profilebutton.click();
+        waitSomeTime(3);
+        /**   profilebutton.click();
+         waitSomeTime(1);
+         profilebutton.click();
+         waitSomeTime(1);
+         **/
+
+        chromeDriver.navigate().refresh();
+        waitSomeTime(1);
+    }
+
+
+    @Test
+    public void fifthTest(){
+
+
+        WebElement editprofbutton =
+                chromeDriver.findElement(By.cssSelector("#app > div.theme-container.no-sidebar > div.page > div.page-edit"));
+        colorTheElementInRed(editprofbutton);
+        editprofbutton.click();
+        waitSomeTime(2);
+        //
+    }
+
+
+
+
+
+    private void waitSomeTime(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void colorTheElementInRed(final WebElement webElement) {
+
+        chromeDriver.executeScript("arguments[0].style.border='3px solid red'", webElement);
+    }
+
+    public void colorTheElementInGreen(final WebElement webElement){
+        chromeDriver.executeScript("arguments[0].style.border='3px solid green'", webElement);
     }
 
     public void colorTheElement(final WebElement webElement, String color) {
